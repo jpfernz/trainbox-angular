cc15ffce14ea7d095699b3dc3308f3363b1983da
jpfernz <jfwintersdev@gmail.com>
2021-08-15 17:25:29 +0100

    Using template variables.

--- src/app/events/event-thumbnail.components.ts
+++ src/app/events/event-thumbnail.components.ts
@@ -19,4 +19,10 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 export class EventThumbnailComponent{
   @Input()
   event: any
+
+  someProperty:any = "some value"
+
+  logFoo() {
+    console.log('foo')
+  }
 }
