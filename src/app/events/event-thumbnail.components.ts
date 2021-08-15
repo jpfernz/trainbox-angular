7de3155815ab8d71903850b5ac15fcfcc0029361
jpfernz <jfwintersdev@gmail.com>
2021-08-15 17:51:36 +0100

    CSS encapsulation

--- src/app/events/event-thumbnail.components.ts
+++ src/app/events/event-thumbnail.components.ts
@@ -10,11 +10,10 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
       <div>Price: \${{event.price}}</div>
       <div>
         <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
       </div>
     </div>
-  `
+  `,
 })
 export class EventThumbnailComponent{
   @Input()
